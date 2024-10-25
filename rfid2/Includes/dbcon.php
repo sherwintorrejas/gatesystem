6ff9009951c76aaea83df0b31998bf1f4a0e36b2
sherwintorrejas <125838306+sherwintorrejas@users.noreply.github.com>
2024-10-25 18:43:04 +0800

    Update dbcon.php

--- rfid2/Includes/dbcon.php
+++ rfid2/Includes/dbcon.php
@@ -1,10 +1,13 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db = "gatesystem";
+// Fetch database configuration from environment variables
+$server = getenv('DB_HOST');          // Fetches DB_HOST from environment variables
+$dbname = getenv('DB_DATABASE');      // Fetches DB_DATABASE from environment variables
+$dbuser = getenv('DB_USERNAME');      // Fetches DB_USERNAME from environment variables
+$dbpass = getenv('DB_PASSWORD');      // Fetches DB_PASSWORD from environment variables
+$dbport = getenv('DB_PORT') ?: '3306'; // Fetches DB_PORT from environment variables or defaults to 3306
 
-$conn = new mysqli($host, $user, $pass, $db);
+// Create a new mysqli instance with the provided credentials
+$conn = new mysqli($server, $dbuser, $dbpass, $dbname, $dbport);
 if ($conn->connect_error) {
 	echo "Seems like you have not configured the database. Failed To Connect to database:" . $conn->connect_error;
 }
